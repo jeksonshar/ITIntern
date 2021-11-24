8bc07f195ffc2c8b86b67c91e8464e129470637f
Your Name <you@example.com>
2021-11-24 20:16:02 +0200

    using DataStore and ViewBinding, save values: loaded not at first time, checkboxes, texts

--- app/src/main/java/com/example/internitcraft/TestDataStore.kt
+++ app/src/main/java/com/example/internitcraft/TestDataStore.kt
@@ -1,4 +1,14 @@
 package com.example.internitcraft
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
 class TestDataStore {
+
+
 }
